0b7daddb89f5c6bfb52845d467d16810cbaa34a2
Juan Pérez Marcelo <junpemarc@gmail.com>
2025-10-16 12:01:49 -0600

    Se realiza actividad

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,3 +1,26 @@
 public class PasswordValidator {
+    public boolean isValid(String password){
+        if(password == null || password.length()<6){
+            return false;
+        }
+        boolean tieneMayus=false;
+        boolean tieneNumero=false;
+        boolean tieneEspChar=false;
+        final String caracteres_Especiales="!@#%^&*$";
 
-}
+        for (char c : password.toCharArray()){
+            if (Character.isUpperCase(c)){
+                tieneMayus=true;
+            } else if (Character.isDigit(c)){
+                tieneNumero=true;
+            }else if (caracteres_Especiales.indexOf(c)>=0){
+                tieneEspChar=true;
+            }
+
+            if(tieneMayus && tieneNumero && tieneEspChar){
+                break;
+            }
+        }//Fin for
+        return tieneMayus && tieneNumero && tieneEspChar;
+    }//Fin clase isValid
+}//Fin clase PasswordValidator
